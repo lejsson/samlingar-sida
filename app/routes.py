a9d2e55b1ac94f6e38ba44cff027ce509ad33242
Ivar <ivar@E7440.localdomain>
2023-05-06 12:11:02 +0200

    database, migrations

--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,11 @@ from flask import render_template, request, url_for, redirect, flash
 from app import app, data
 from app.forms import LoginForm
 
-# import data #Funkar ej att importera data.py
-
-list = data.test
-
 @app.route('/')
-@app.route('/index')
+@app.route('/index/')
 def index():
-    return render_template('index.html', title='Home', user=data.user, posts=data.posts)
+    # return render_template('index.html', title='Home', user=data.user, posts=data.posts)
+    return render_template('index.html', title='Home', posts=data.posts)
 
 @app.route('/collections/')
 def collections():
@@ -21,5 +18,6 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        # return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
