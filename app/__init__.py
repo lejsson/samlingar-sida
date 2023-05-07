8ce1df61a7b81cea4075ef67f75b3d7730daa8bb
Ivar <ivar@E7440.localdomain>
2023-05-07 10:28:15 +0200

    Login system, småsaker, gitignore för venv och pycache

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+login = LoginManager(app)
 
 from app import routes, models
