cecb85103e64f6b8a3baa536813efcfc97f1a785
Ivar <ivar@E7440.localdomain>
2023-05-21 22:38:56 +0200

    main.py fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app import app, db
 from app.models import User, Post, PostContent
-app.run(debug=False)
+if __name__ == '__main__':
+    app.run(debug=False)
 
 @app.shell_context_processor
 def make_shell_context():
